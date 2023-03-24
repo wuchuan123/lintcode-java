c4d470e0533c22ff35c523b2ffd35c870bd50fdb
wuchuan <609833577@qq.com>
2023-03-24 18:17:10 +0800

    练习

--- src/main/java/com/github/wuchuan123/三和三的倍数.java
+++ src/main/java/com/github/wuchuan123/三和三的倍数.java
@@ -6,6 +6,7 @@ import java.util.stream.Collectors;
 public class 三和三的倍数 {
   public List<Integer> removeNumber(List<Integer> list) {
 //    收集器（Collector）是为Stream.collect()方法量身打造的工具接口（类）。
+//    流的 collect 就配合 Collectors 使用 是一个定式
     return list.stream().filter(item -> item % 3 != 0).collect(Collectors.toList());
   }
 }
